6101de975818bc4901c685296a15e39e8efcf75f
dkarp <dkarputs@gmail.com>
2024-08-05 23:40:49 +0200

    Initial commit

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=postgres',
-    'username' => 'postgres',
-    'password' => 'postgres',
+    'dsn' => 'pgsql:host=vultr-prod-622a341a-efb6-4711-845f-85d0246a7fcb-vultr-prod-15e1.vultrdb.com;port=16751;dbname=defaultdb',
+    'username' => 'vultradmin',
+    'password' => 'AVNS_Rdq4NAkuIc5jqlp-b84',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
