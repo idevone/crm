f6a76f7cf267774ec4c7b971323a840963e2e690
dkarp <dkarputs@gmail.com>
2024-08-09 23:28:49 +0200

    Update db connect to vultr db

--- config/db.php
+++ config/db.php
@@ -3,13 +3,13 @@
 return [
     'class' => 'yii\db\Connection',
 
-//    'dsn' => 'pgsql:host=vultr-prod-622a341a-efb6-4711-845f-85d0246a7fcb-vultr-prod-15e1.vultrdb.com;port=16751;dbname=defaultdb',
-//    'username' => 'vultradmin',
-//    'password' => 'AVNS_Rdq4NAkuIc5jqlp-b84',
+    'dsn' => 'pgsql:host=vultr-prod-622a341a-efb6-4711-845f-85d0246a7fcb-vultr-prod-15e1.vultrdb.com;port=16751;dbname=defaultdb',
+    'username' => 'vultradmin',
+    'password' => 'AVNS_Rdq4NAkuIc5jqlp-b84',
 //    'dsn' => 'pgsql:host=localhost;dbname=postgres',
 
-    'dsn' => 'pgsql:host=pgsql;port=5432;dbname=postgres',
-    'username' => 'postgres',
-    'password' => 'postgres',
+//    'dsn' => 'pgsql:host=pgsql;port=5432;dbname=postgres',
+//    'username' => 'postgres',
+//    'password' => 'postgres',
     'charset' => 'utf8',
 ];
